47760f07420e5e9d1a61e60f9755af4b8ee74c6c
Ed Kelley <ekelleyv@gmail.com>
2015-01-29 16:23:30 -0500

    Loading stadium

--- js/app.js
+++ js/app.js
@@ -8,12 +8,71 @@ World.prototype.init = function() {
     this.scene = this.init_scene();
     this.camera = this.init_camera();
 
-
-    this.build_stadium();
+    this.load_geo();
+    this.load_listings();
 
     window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
     requestAnimationFrame(this.render.bind(this));
 
+};
+
+World.prototype.load_geo = function() {
+    var that = this;
+    $.getJSON("/data/yankees_geo.json", function(data) {
+        console.log("Received geo json");
+        that.geo_data = data;
+        that.process_mapdata();
+    });
+};
+
+World.prototype.load_listings = function() {
+    var that = this;
+    $.getJSON("/data/yankees_listings.json", function(data) {
+        console.log("Received listings json");
+        that.listings_data = data;
+        that.process_mapdata();
+    });
+};
+
+World.prototype.process_mapdata = function() {
+    if (!this.geo_data || !this.listings_data) {
+        return;
+    }
+    this.mapdata = {};
+
+    var sections = this.geo_data.sections;
+
+    for (var section_name in sections) {
+        var section = sections[section_name];
+        this.mapdata[section_name] = {
+            center : section.center,
+            points : section.points[0],
+            listings: []
+        }
+    }
+
+    for (var i = 0; i < this.listings_data.listings.length; i++) {
+        var listing = this.listings_data.listings[i];
+        if (listing.mk) {
+            var re = /^s:((?:\w|-)+)/;
+            var section_name = re.exec(listing.mk)[1];
+
+            if (!this.mapdata[section_name]) { continue; };
+
+            this.mapdata[section_name].listings.push({
+                dq_bucket : listing.b,
+                dq : listing.dq
+            });
+
+            if (!this.mapdata[section_name].max_dq || listing.dq > this.mapdata[section_name].max_dq) {
+                this.mapdata[section_name].max_dq = listing.dq;
+                this.mapdata[section_name].max_dq_bucket = listing.b;
+                this.mapdata[section_name].max_dq_price = listing.pf;
+            }
+        };
+    }
+
+    this.build_stadium();
 };
 
 World.prototype.init_renderer = function() {
@@ -38,8 +97,8 @@ World.prototype.onWindowResize = function() {
 
 World.prototype.init_scene = function() {
     var scene = new THREE.Scene({ fixedTimeStep: 1 / 120 });
-    scene.fog = new THREE.Fog( 0xffffff, 1, 7000 );
-    scene.fog.color.setHSL( 0.6, 0, 1 );
+    // scene.fog = new THREE.Fog( 0xffffff, 1, 7000 );
+    // scene.fog.color.setHSL( 0.6, 0, 1 );
     return scene;
 };
 
@@ -55,9 +114,8 @@ World.prototype.init_camera = function() {
                                 ASPECT,
                                 NEAR,
                                 FAR  );
-    // camera.position.set( 0, 50, 0);
+    camera.position.set( 0, -75, 100);
     camera.lookAt(new THREE.Vector3(0, 0, 0) );
-    camera.h_rotation = 0;
 
     this.scene.add(camera);
 
@@ -66,28 +124,35 @@ World.prototype.init_camera = function() {
 
 
 World.prototype.build_stadium =  function () {
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshNormalMaterial();
-    var cube = new THREE.Mesh( geometry, material );
-    this.scene.add( cube );
-    this.camera.position.z = -5;
-    this.camera.position.y = 5;
-    this.camera.position.x = -5;
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0) );
+    this.stadium_group = new THREE.Object3D();
+    var building_material = new THREE.MeshNormalMaterial();
+
+    for (var section_name in this.mapdata) {
+        var section = this.mapdata[section_name];
+        var shape = this.convert_shape(section.points);
+        var geometry = shape
+          .extrude({
+            amount: 0.35,
+            bevelEnabled: false
+          });
+        var object = new THREE.Mesh(geometry, building_material);
+        // object.position.y = this.mapdata[section_name].max_dq || 0;
+        object.castShadow = true;
+        object.receiveShadow = true;
+        this.stadium_group.add(object);
+    }
+
+    this.scene.add(this.stadium_group);
 };
 
-World.prototype.convert_shape = function (geometry) {
-      if (geometry.type !== "Polygon") {
-        throw "Only Polygons are currently supported";
-      }
-      var vertices = geometry.coordinates[0];
-      var pts = [];
-      for (var i = 0; i < vertices.length; ++i) {
-        pts.push(new THREE.Vector2(vertices[i][1], vertices[i][0]));
-      }
-      var shape = new THREE.Shape();
-      shape.fromPoints(pts);
-      return shape;
+World.prototype.convert_shape = function (input_points) {
+    var pts = [];
+    for (var i = 0; i < input_points.length; i++) {
+        pts.push(new THREE.Vector2((input_points[i][0]-500)/10, (500 - input_points[i][1])/10));
+    }
+    var shape = new THREE.Shape();
+    shape.fromPoints(pts);
+    return shape;
 },
 
 World.prototype.render = function() {
@@ -101,10 +166,3 @@ World.prototype.render = function() {
 var world = new World();
 
 window.addEventListener("load", world.init.bind(world));
-
-// setTimeout(function () {
-//   $.get("citi-field.geo.json", function (data) {
-//     world.data = data.features;
-//     world.build_stadium();
-//   });
-// }, 500);
